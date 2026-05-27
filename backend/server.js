8efd4af10232e967222303e38e482805a8c98061
zunii66 <javediqbal182006@gmail.com>
2026-05-27 20:08:24 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,69 @@
 
+require('dotenv').config();
+
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: process.env.CALLBACK_URL,
+    profileFields: ['id', 'displayName']
+  },
+  function(accessToken, refreshToken, profile, done) {
+
+    return done(null, profile);
+  }
+));
+
+app.get('/', (req, res) => {
+  res.send('Backend Running');
+});
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook')
+);
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {
+    failureRedirect: '/'
+  }),
+  (req, res) => {
+
+    res.send(`
+      <h1>Facebook Login Success</h1>
+      <p>Name: ${req.user.displayName}</p>
+      <p>ID: ${req.user.id}</p>
+    `);
+  }
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log('Server Started');
+});
