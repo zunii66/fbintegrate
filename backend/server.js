d491076263738c4fdfdbe978714e33e6edd6cf9d
zunii66 <javediqbal182006@gmail.com>
2026-05-27 22:00:35 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
@@ -19,60 +22,53 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-
-passport.deserializeUser((obj, done) => {
-  done(null, obj);
-});
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
 
 passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: process.env.CALLBACK_URL,
-    profileFields: ['id', 'displayName', 'photos']
-  },
-  function(accessToken, refreshToken, profile, done) {
-
-    return done(null, profile);
-  }
-));
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: process.env.CALLBACK_URL,
+  profileFields: ['id', 'displayName', 'photos']
+},
+(accessToken, refreshToken, profile, done) => {
+  return done(null, profile);
+}));
 
 app.get('/', (req, res) => {
   res.send('Backend Running');
 });
 
 app.get('/auth/facebook',
-  passport.authenticate('facebook')
+  passport.authenticate('facebook', {
+    scope: ['email']
+  })
 );
 
 app.get('/auth/facebook/callback',
   passport.authenticate('facebook', {
-    failureRedirect: '/'
+    failureRedirect: 'https://zunii66.github.io/fbintegrate/'
   }),
   (req, res) => {
 
-    const name = encodeURIComponent(req.user.displayName);
-    const id = req.user.id;
-
-    let photo = '';
-
-    if (
-      req.user.photos &&
-      req.user.photos.length > 0
-    ) {
-      photo = encodeURIComponent(req.user.photos[0].value);
+    if (!req.user) {
+      return res.redirect('https://zunii66.github.io/fbintegrate/');
     }
 
-    res.redirect(
-      `https://zunii66.github.io/fbintegrate/dashboard.html?name=${name}&id=${id}&photo=${photo}`
+    const name = encodeURIComponent(req.user.displayName || "User");
+    const id = req.user.id;
+
+    const photo =
+      req.user.photos?.[0]?.value ||
+      `https://graph.facebook.com/${id}/picture?type=large`;
+
+    return res.redirect(
+      `https://zunii66.github.io/fbintegrate/dashboard.html?name=${name}&id=${id}&photo=${encodeURIComponent(photo)}`
     );
   }
 );
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log('Server Started');
 });
